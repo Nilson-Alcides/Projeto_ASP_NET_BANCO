d4c18bf18fb615cabc276119b2c0286844ddda98
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-06 21:18:14 -0300

    Update projeto

--- Projeto_BancoDados/Repository/Contract/IUsuarioRepository.cs
+++ Projeto_BancoDados/Repository/Contract/IUsuarioRepository.cs
@@ -1,8 +1,13 @@
-﻿namespace Projeto_BancoDados.Repository.Contract
+﻿using Projeto_BancoDados.Models;
+
+namespace Projeto_BancoDados.Repository.Contract
 {
     public interface IUsuarioRepository
     {
         //CRUD
-
+        IEnumerable<Usuario> ObterTodosUsuarios();
+        void CadastrarUsuario(Usuario usuario);
+        void AtualizarUsuario(Usuario usuario);
+        Usuario ObterUsuario(int  id);
     }
 }
--- Projeto_BancoDados/obj/Debug/net10.0/Projeto_BancoDados.AssemblyInfo.cs
+++ Projeto_BancoDados/obj/Debug/net10.0/Projeto_BancoDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_BancoDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc63e83c93275bebd82ce7fd3ab35b77ff5432c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_BancoDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_BancoDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
