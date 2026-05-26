ce945dbc5dd9ea84985176479f0504349e470a3d
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-05-26 20:21:54 -0300

    Atualizar usuario

--- Projeto_BancoDados/Program.cs
+++ Projeto_BancoDados/Program.cs
@@ -1,7 +1,12 @@
+using Projeto_BancoDados.Repository;
+using Projeto_BancoDados.Repository.Contract;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
--- Projeto_BancoDados/Repository/UsuarioRepository.cs
+++ Projeto_BancoDados/Repository/UsuarioRepository.cs
@@ -13,9 +13,23 @@ namespace Projeto_BancoDados.Repository
         {
             _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");  
         }
+        
         public void AtualizarUsuario(Usuario usuario)
         {
-            throw new NotImplementedException();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Update usuario set nomeUsu=@nomeUsu, Cargo=@Cargo, " +
+                                                    " DataNasc=@DataNasc Where IdUsu=@IdUsu;", conexao);
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.cargo;
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.dataNasc.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@IdUsu", MySqlDbType.VarChar).Value = usuario.idUsu;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
         }
 
         public void CadastrarUsuario(Usuario usuario)
@@ -39,12 +53,56 @@ namespace Projeto_BancoDados.Repository
 
         public IEnumerable<Usuario> ObterTodosUsuarios()
         {
-            throw new NotImplementedException();
-        }
+            List<Usuario> UsuarioList = new List<Usuario>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from usuario", conexao);
 
-        public Usuario ObterUsuario(int id)
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Clone();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UsuarioList.Add(
+                        new Usuario
+                        {
+                            idUsu = Convert.ToInt32(dr["IdUsu"]),
+                            nomeUsu = (string)dr["nomeUsu"],
+                            cargo = (string)dr["Cargo"],
+                            dataNasc = Convert.ToDateTime(dr["DataNasc"])
+
+                        });
+                }
+                return UsuarioList;
+            }
+        }
+        public Usuario ObterUsuario(int Id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * from usuario " +
+                                                    " where IdUsu=@IdUsu", conexao);
+                cmd.Parameters.AddWithValue("@IdUsu", Id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Usuario usuario = new Usuario();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+                    usuario.idUsu = Convert.ToInt32(dr["IdUsu"]);
+                    usuario.nomeUsu = (string)(dr["nomeUsu"]);
+                    usuario.cargo = (string)(dr["Cargo"]);
+                    usuario.dataNasc = Convert.ToDateTime(dr["DataNasc"]);
+                }
+                return usuario;
+            }
         }
     }
 }
